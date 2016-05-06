cf5e64c4773a5dd6cd82ddadb9699cd0d1261ef8
Emanuil Dobrev <edobre4@gmail.com>
2016-05-06 16:14:59 -0500

    added locksreen button functionality

--- app/src/main/java/com/example/ivan/facelock/LockscreenActivity.java
+++ app/src/main/java/com/example/ivan/facelock/LockscreenActivity.java
@@ -1,9 +1,16 @@
 package com.example.ivan.facelock;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -25,9 +32,11 @@ public class LockscreenActivity extends AppCompatActivity {
     private String mPin;
     private boolean mClock;
     private String mBackground;
+    private Context mContext;
 
     TextView timeTextView;
     TextView dateTextView;
+    TextView pinTextView;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +57,14 @@ public class LockscreenActivity extends AppCompatActivity {
         ImageButton back = (ImageButton) findViewById(R.id.buttonBack);
         ImageButton ok = (ImageButton) findViewById(R.id.buttonOK);
 
-        TextView pinTextView = (TextView) findViewById(R.id.pinTextView);
+        pinTextView = (TextView) findViewById(R.id.pinTextView);
         TextView enterPinTextView = (TextView) findViewById(R.id.pinTextView);
         timeTextView = (TextView) findViewById(R.id.timeTextView);
         dateTextView = (TextView) findViewById(R.id.dateTextView);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.linear_layout);
 
-
+        mContext = this;
         mEnteredPin = "";
 
         // get settings from preferences file
@@ -65,7 +74,110 @@ public class LockscreenActivity extends AppCompatActivity {
         mBackground = mPreferences.getString("background", "");
         mClock = mPreferences.getBoolean("clock", false);
 
+        pinTextView.setText("");
         updateTime();
+
+        if (!mBackground.equals("Default"))
+            layout.setBackground(Drawable.createFromPath(mBackground));
+
+        // set a listener for each button
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('1');
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('2');
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('3');
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('4');
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('5');
+            }
+        });
+
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('6');
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('7');
+            }
+        });
+
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('8');
+            }
+        });
+
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('9');
+            }
+        });
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addChar('0');
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEnteredPin.length() > 0) {
+                    // vibrate
+                    Vibrator vibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+                    vibrator.vibrate(500);
+
+                    // make a click sound
+                    MediaPlayer mp = MediaPlayer.create(mContext, R.raw.click);
+                    mp.start();
+
+                    mEnteredPin = mEnteredPin.substring(0, mEnteredPin.length() - 2);
+                    pinTextView.setText(pinTextView.getText().toString().substring(0, pinTextView.getText().toString().length() - 2));
+                }
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEnteredPin.equals(mPin)) {
+                    // unlock phone
+
+                }
+            }
+        });
     }
 
     private void updateTime() {
@@ -85,5 +197,24 @@ public class LockscreenActivity extends AppCompatActivity {
             timeTextView.setText("");
             dateTextView.setText("");
         }
+    }
+
+    private void addChar(char c) {
+        // max pin length is 10
+        if (mEnteredPin.length() > 10)
+            return;
+
+        // vibrate
+        Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+
+        // make a click sound
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
+        mp.start();
+
+        // append the character
+        mEnteredPin = mEnteredPin + c;
+
+        pinTextView.setText(pinTextView.getText().toString() + '*');
     }
 }
